d39d310cd6f80d2e87b860ffcfb93481444f202c
Jian De <jiande2020@protonmail.com>
2026-09-07 20:46:52 +0800

    fix: subscribe to property changes before media discovery

--- native/include/media_object_manager.h
+++ native/include/media_object_manager.h
@@ -25,7 +25,6 @@ class MediaObjectManager {
                            const InterfacesMap& interfaces);
   void on_interfaces_removed(const sdbus::ObjectPath& object_path,
                              const std::vector<std::string>& interfaces);
-  void subscribe_properties(const std::string& object_path);
   void post_properties(const std::string& object_path,
                        const std::string& interface_name);
   void post_removed(const std::string& object_path,
@@ -34,7 +33,7 @@ class MediaObjectManager {
   sdbus::IConnection& conn_;
   Dart_Port_DL events_port_;
   std::unique_ptr<sdbus::IProxy> root_proxy_;
-  std::map<std::string, std::unique_ptr<sdbus::IProxy>> property_proxies_;
+  sdbus::Slot properties_subscription_;
   std::map<std::string, std::set<std::string>> interfaces_by_path_;
   std::map<std::string, InterfacesMap> properties_by_path_;
 };
